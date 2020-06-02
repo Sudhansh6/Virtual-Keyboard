b362719d278099d2edb268dc7662403e2926cbd9
Sudhansh6 <52414199+Sudhansh6@users.noreply.github.com>
2020-06-02 20:04:40 +0530

    Invisibility cloak simulator

--- cloak.py
+++ cloak.py
@@ -3,10 +3,7 @@ import numpy as np
 
 def nothing(x) : pass
 
-cap = cv2.VideoCapture(-1) # Start Livefeed
-
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc,20,(640,480))
+cap = cv2.VideoCapture(0) # Start Livefeed
 
 cv2.waitKey(3000) # Wait for three seconds and take background picture
 _,pic = cap.read()
@@ -17,20 +14,20 @@ cv2.namedWindow('frame')
 try:
 	f = open('hsv_values.txt','r') # check if hsv values are present
 	a = list(int(x.replace('\n','')) for x in f)
-	H,S,V,h,s,v = a
+	h,s,v,H,S,V = a
 	f.close()
 except:
 	H,S,V,h,s,v = 0,0,0,0,0,0
 	pass
-	
+		
 
-cv2.createTrackbar('h_low','frame',H,180,nothing)
-cv2.createTrackbar('s_low','frame',S,255,nothing)
-cv2.createTrackbar('v_low','frame',V,255,nothing)
+cv2.createTrackbar('h_low','frame',h,180,nothing)
+cv2.createTrackbar('s_low','frame',s,255,nothing)
+cv2.createTrackbar('v_low','frame',v,255,nothing)
 
-cv2.createTrackbar('h_high','frame',h,180,nothing) # create trackbars for h,s,v values
-cv2.createTrackbar('s_high','frame',s,255,nothing)
-cv2.createTrackbar('v_high','frame',v,255,nothing)
+cv2.createTrackbar('h_high','frame',H,180,nothing) # create trackbars for h,s,v values
+cv2.createTrackbar('s_high','frame',S,255,nothing)
+cv2.createTrackbar('v_high','frame',V,255,nothing)
 
 while(cap.isOpened()):
 
@@ -52,15 +49,15 @@ while(cap.isOpened()):
 	frame = cv2.cvtColor(frame,cv2.COLOR_HSV2BGR) # convert back to bgr to display
 
 	cv2.imshow('frame',frame)
-	out.write(frame) # Write to output file
+	cv2.imshow('mask',mask)
 
 	k = cv2.waitKey(50) & 0XFF
 	if(k == ord('q')): 
-		out.release()
 		break
 f = open('hsv_values.txt','w+')
-f.write(str(H)+'\n'+str(S)+'\n'+str(V)+'\n') # store the values in a file
+ # store the values in a file
 f.write(str(h)+'\n'+str(s)+'\n'+str(v)+'\n')
+f.write(str(H)+'\n'+str(S)+'\n'+str(V)+'\n')
 f.close()
 
 cap.release()
